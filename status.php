f6742f7c3f82a35b69aec8b4b7982e17500e94d5
Troner <trontzo@gmail.com>
2017-03-05 17:06:03 +0100

    Update status.php

--- status.php
+++ status.php
@@ -1,26 +1,20 @@
 <?php
-/** LOAD TEAMSPEAK FRAMEWORK **/
+//load the Teamspeak framework
 require_once('/path/to/the/needed/libary/ts3_lib/TeamSpeak3.php'); #EDIT THIS
 
-/** CONNECT TO TEAMSPEAK WITH QUERY **/
+//build a query connection to the Teamspeak server
 $ts3 = TeamSpeak3::factory("serverquery://QUERYNAME:QUERYPASSWORD@SERVERIP:QUERYPORT/?server_port=SERVERPORT"); #EDIT ALL THAT'S WRITTEN IN CAPSLOCK
 
-/** FAKE TO PNG FILE **/
+//Say to Teamspeak that's a image file
 header("Content-type: image/png");
 
-if($ts3->virtualserver_queryclientsonline < 1) {
-	$status = "offline";
-} else {
-	$status = "online";
-} 
-
-/** BANNER STRING **/
+//String that will be printed on the banner
 $string = date("H:i") . " Uhr |" . " Online: " . $ts3->virtualserver_clientsonline . "/" . $ts3->virtualserver_maxclients;
 
-/** CHECK WHICH DAY **/
-$image = "images/example.png"; #PATH TO THE BANNER (PNG)
+//select the image 
+$image = "images/example.png"; #change the path to your wished banner image (must be .png)
 
-/** PRINT DATA ON PNG **/
+//Print the string on the png
 $im     = imagecreatefrompng($image);
 $color = imagecolorallocate($im, 255, 255, 255); #You can change the RGB code here 
 $px     = (imagesx($im) - 7.5 * strlen($string)) / 25;
